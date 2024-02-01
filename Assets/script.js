6764c1c1c82beb3c4a0b50e9e444980b4082bd73
dfussell1 <daltonfussell1@gmail.com>
2024-01-31 20:25:56 -0600

    changed styling a little, created loop to compare hours for adding classes to timeblocks, added saving functionality to timeblock descriptions

--- Assets/script.js
+++ Assets/script.js
@@ -3,6 +3,26 @@
 // in the html.
 
 $(function () {
+  $('.saveBtn').on('click', saveDesc);
+
+  function saveDesc() {
+    localStorage.setItem($(this).parent().attr('id'), $(this).siblings('.description').val());
+  };
+  
+  var timeBlockEl = $('.time-block');
+  
+  timeBlockEl.each(function() {
+    var currentHour = parseInt($(this).attr('id'));
+    var calHour = dayjs().hour();
+    parseInt($(this).attr('id'));
+    if (currentHour < calHour) {
+      $(this).addClass('past');
+    } else if (currentHour === calHour) {
+      $(this).addClass('present');
+    } else {
+      $(this).addClass('future');
+    }
+  });
 });
 
 function displayTime() {
@@ -27,5 +47,30 @@ function displayTime() {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+$("#0 .description").val(localStorage.getItem('0'));
+$("#1 .description").val(localStorage.getItem('1'));
+$("#2 .description").val(localStorage.getItem('2'));
+$("#3 .description").val(localStorage.getItem('3'));
+$("#4 .description").val(localStorage.getItem('4'));
+$("#5 .description").val(localStorage.getItem('5'));
+$("#6 .description").val(localStorage.getItem('6'));
+$("#7 .description").val(localStorage.getItem('7'));
+$("#8 .description").val(localStorage.getItem('8'));
+$("#9 .description").val(localStorage.getItem('9'));
+$("#10 .description").val(localStorage.getItem('10'));
+$("#11 .description").val(localStorage.getItem('11'));
+$("#12 .description").val(localStorage.getItem('12'));
+$("#13 .description").val(localStorage.getItem('13'));
+$("#14 .description").val(localStorage.getItem('14'));
+$("#15 .description").val(localStorage.getItem('15'));
+$("#16 .description").val(localStorage.getItem('16'));
+$("#17 .description").val(localStorage.getItem('17'));
+$("#18 .description").val(localStorage.getItem('18'));
+$("#19 .description").val(localStorage.getItem('19'));
+$("#20 .description").val(localStorage.getItem('20'));
+$("#21 .description").val(localStorage.getItem('21'));
+$("#22 .description").val(localStorage.getItem('22'));
+$("#23 .description").val(localStorage.getItem('23'));
+
 displayTime();
 setInterval(displayTime, 1000);
